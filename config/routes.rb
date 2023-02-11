3e26a7dea3d7139df617776b25ad883b8ebcb11e
Rick Yellow <yenju0425@gmail.com>
2023-02-11 17:44:07 +0800

    create new page (about)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  # get 'home/index'
+  get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
